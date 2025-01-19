c6112e65babed3ff1cfe9fb88a38b9eb99cfea56
Gian <108510905+RaiGZ971@users.noreply.github.com>
2025-01-20 03:52:50 +0800

    Registration 99%

--- SeedSync/src/application/Main.java
+++ SeedSync/src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 	
+import application.controller.DatabaseQueries;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
---
+++ SeedSync/src/application/controller/DatabaseQueries.java
@@ -0,0 +1,119 @@
+package application.controller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DatabaseQueries {
+	private DatabaseConnection connectNow = new DatabaseConnection();
+	private Connection connectDatabase = connectNow.getConnection();
+	
+	private String query;
+	
+	private String userID;
+	
+	public int getAccountCount(String accountType) {
+		int count = 0;
+		
+		query = "SELECT COUNT(user_id) AS count "
+				+ "FROM users_account "
+				+ "WHERE account_type = ?";
+		
+		try {
+			PreparedStatement preparedStatement = connectDatabase.prepareStatement(query);
+			
+			preparedStatement.setString(1, accountType);
+			
+			ResultSet result = preparedStatement.executeQuery();
+			
+			if(result.next()) {
+				count = result.getInt("count");
+			}else {
+				System.out.println("No account Found");
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		return count;
+	}
+	
+	public String generateUserID(String accountType) {
+		int userNumber = getAccountCount(accountType) + 1;
+		String userID = "";
+		
+		if(accountType == "Farmer")
+			userID = String.format("FA%03d", userNumber);
+		else if(accountType == "Buyer")
+			userID = String.format("BY%03d", userNumber);
+		else if (accountType == "Middle Man")
+			userID = String.format("MM%03d", userNumber);
+		return userID;
+	}
+	
+	public void registerAccountDBS(String email, String username, String password, String accountType) {
+		userID = generateUserID(accountType);
+		
+		query = "INSERT INTO users_account(user_id, email, username, password, account_type) "
+				+ "VALUES (?, ?, ?, ?, ?)";
+		
+		try {
+			PreparedStatement preparedStatement = connectDatabase.prepareStatement(query);
+			
+			preparedStatement.setString(1, userID);
+			preparedStatement.setString(2, email);
+			preparedStatement.setString(3, username);
+			preparedStatement.setString(4, password);
+			preparedStatement.setString(5, accountType);
+			
+			preparedStatement.executeUpdate();			
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public void registerAccountDetailDBS(String firstName, String lastName, String birthdate, int age, String phoneNumber) {
+		query = "INSERT INTO users_detail(firstname, lastname, birthdate, age, phone_number, user_id) "
+				+ "VALUES (?, ?, ?, ?, ?, ?)";
+		
+		try {
+			PreparedStatement preparedStatement = connectDatabase.prepareStatement(query);
+			
+			preparedStatement.setString(1, firstName);
+			preparedStatement.setString(2, lastName);
+			preparedStatement.setString(3, birthdate);
+			preparedStatement.setInt(4, age);
+			preparedStatement.setString(5, phoneNumber);
+			preparedStatement.setString(6, userID);
+			
+			preparedStatement.executeUpdate();			
+			
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public void registerAccountAddressDBS(String region, String province, String city, String address) {
+		query = "INSERT INTO users_address(region, province, city, address, user_id) "
+				+ "VALUES (?, ?, ?, ?, ?)";
+		
+		try {
+			PreparedStatement preparedStatement = connectDatabase.prepareStatement(query);
+			
+			preparedStatement.setString(1, region);
+			preparedStatement.setString(2, province);
+			preparedStatement.setString(3, city);
+			preparedStatement.setString(4, address);
+			preparedStatement.setString(5, userID);
+			
+			preparedStatement.executeUpdate();			
+			
+			System.out.println("GOod");
+			
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	
+}
