1cb33be9e45221c59d388bbd0f61cd50379126c5
mayellaaa <mariellasanchez03@gmail.com>
2025-01-27 22:09:48 +0800

    finished log in

--- SeedSync/src/application/controller/LoginController.java
+++ SeedSync/src/application/controller/LoginController.java
@@ -2,6 +2,7 @@ package application.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -12,14 +13,25 @@ public class LoginController extends DatabaseQueries{
 	@FXML
 	private PasswordField loginPasswordFX;
 	
+	private Label loginVerificationFX;
+	
 	public void loginAccount(ActionEvent event) {
 		String loginUsernameEmail = loginUsernameEmailFX.getText();
-		
+		String loginPassword = loginPasswordFX.getText();
 		String userID = searchAccountDBS(loginUsernameEmail);
 		
 		if(userID.isEmpty()) {
-			//no account found
+			loginUsernameEmailFX.setStyle("-fx-opacity: 1; -fx-text-fill: red");
+			loginVerificationFX.setText("Account not found");
 		}else {
+			boolean verifyPassword = verifyPasswordDBS(userID, loginPassword);
+			if(verifyPassword) {
+				loginVerificationFX.setStyle("-fx-opacity: 1; -fx-text-fill: -fx-dark-primary-color");
+				loginVerificationFX.setText("Login Sucessful!");
+			}else {
+				loginPasswordFX.setStyle("-fx-opacity: 1; -fx-text-fill: red");
+				loginVerificationFX.setText("Incorrect Password");
+			}
 			System.out.println(userID);
 		}
 	}
