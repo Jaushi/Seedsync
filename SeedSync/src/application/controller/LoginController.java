a412d927b24b15c5e98520c19a6c3d8748ba2f20
Gian <108510905+RaiGZ971@users.noreply.github.com>
2025-01-28 12:35:41 +0800

    habol

--- SeedSync/src/application/controller/LoginController.java
+++ SeedSync/src/application/controller/LoginController.java
@@ -1,27 +1,48 @@
 package application.controller;
 
+import javafx.scene.input.MouseEvent;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-public class LoginController extends DatabaseQueries{
+
+public class LoginController extends UniversalController{
 	
 	@FXML
 	private TextField loginUsernameEmailFX;
 	@FXML
 	private PasswordField loginPasswordFX;
+	@FXML
+	private Label loginVerificationFX;
 	
 	public void loginAccount(ActionEvent event) {
 		String loginUsernameEmail = loginUsernameEmailFX.getText();
-		
+		String loginPassword = loginPasswordFX.getText();
 		String userID = searchAccountDBS(loginUsernameEmail);
 		
 		if(userID.isEmpty()) {
-			//no account found
+			loginUsernameEmailFX.setStyle("-fx-border-color: red;");
+			loginVerificationFX.setStyle("-fx-opacity: 1;");
+			loginVerificationFX.setText("Account not found");
 		}else {
-			System.out.println(userID);
+			boolean verifyPassword = verifyPasswordDBS(userID, loginPassword);
+			loginUsernameEmailFX.setStyle("-fx-border-color: -fx-dark-primary-color;");
+			if(verifyPassword) {	
+				loginPasswordFX.setStyle("-fx-border-color: -fx-dark-primary-color;");
+				loginVerificationFX.setStyle("-fx-opacity: 1; -fx-text-fill: -fx-light-primary-color");
+				loginVerificationFX.setText("Login Sucessful!");
+			}else {
+				loginPasswordFX.setStyle("-fx-border-color: red;");
+				loginVerificationFX.setText("-fx-opacity: 1;");
+				loginVerificationFX.setText("Incorrect Password");
+			}
 		}
+	}
+	
+	public void clickedRegistration(ActionEvent event) throws Exception {
+		changeToRegistration(event);
 	}
 	
 	
