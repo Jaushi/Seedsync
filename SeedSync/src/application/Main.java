71c765b2f98776dd2a5c9e8bb2750c5cb73008ac
graciasShella <bautistachryshella@gmail.com>
2025-01-28 10:55:19 +0800

    Added seller pages
    -homepage (dashboard)
    -inventory
    -navigation
    -product bar

--- SeedSync/src/application/Main.java
+++ SeedSync/src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("fxml/EditProfile.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("fxml/sellerProductBar.fxml"));
 			
 			Image logo = new Image(getClass().getResource("assets/images/logo.png").toExternalForm());
 			
