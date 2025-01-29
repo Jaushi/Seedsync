3717e64c2771abb011be1ee2571786796b2fbaf8
Gian <108510905+RaiGZ971@users.noreply.github.com>
2025-01-29 18:07:28 +0800

    update

--- SeedSync/src/application/controller/Seller.java
+++ SeedSync/src/application/controller/Seller.java
@@ -1,76 +1,56 @@
 package application.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Seller extends Users {
-    private List<Product> soldItems = new ArrayList<>();
-    private double totalIncome = 0.0;
-
-    Seller() {}
-
-    
-    Seller(String _userID,
-           String _email,
-           String _userName,
-           String _password,
-           String _accountType,
-           String _firstName,
-           String _lastName,
-           String _birthdate,
-           String _phoneNumber,
-           String _region,
-           String _province,
-           String _city,
-           String _address,
-           int _age ) {
-
-        this.userID = _userID;
-        this.email = _email;
+public class Seller extends Users{
+	
+	Seller(){}
+	
+	Seller(String _userID, String _email, String _userName, String _password, String _accountType, String _firstName, String _lastName, String _birthdate, int _age, String _phoneNumber, String _region, String _province, String _city, String _address){
+		this.userID = _userID;
+		this.email = _email;
         this.username = _userName;
         this.password = _password;
-        this.accountType = _accountType;
+        this.accountType = _accountType;	
         this.firstName = _firstName;
-        this.lastName = _lastName;
-        this.birthdate = _birthdate;
-        this.phoneNumber = _phoneNumber;
-        this.age = _age;
-        this.region = _region;
-        this.province = _province;
-        this.city = _city;
-        this.address = _address;
-    }
-
-    public void recordSale(Product product) {
-        soldItems.add(product);
-        calculateIncome(product);
-    }
-
-    public void recordSales(List<Product> products) {
-        soldItems.addAll(products);
-        for (Product product : products) {
-            calculateIncome(product);
-        }
-    }
-
-    private void calculateIncome(Product product) {
-        totalIncome += product.getPrice();
-    }
-
-    public double getTotalIncome() {
-        return totalIncome;
-    }
-
-    public int getTotalItemsSold() {
-        return soldItems.size();
-    }
-
-    public List<Product> getSoldItems() {
-        return new ArrayList<>(soldItems);
-    }
-
-    public void resetIncome() {
-        soldItems.clear();
-        totalIncome = 0.0;
-    }
+		this.lastName = _lastName;
+		this.birthdate = _birthdate;
+		this.phoneNumber = _phoneNumber;
+		this.age = _age;
+		this.region = _region;
+		this.province = _province;
+		this.city = _city;
+		this.address = _address;
+	}
+//
+//    public void recordSale(Product product) {
+//        soldItems.add(product);
+//        calculateIncome(product);
+//    }
+//
+//    public void recordSales(List<Product> products) {
+//        soldItems.addAll(products);
+//        for (Product product : products) {
+//            calculateIncome(product);
+//        }
+//    }
+//
+//    private void calculateIncome(Product product) {
+//        totalIncome += product.getPrice();
+//    }
+//
+//    public double getTotalIncome() {
+//        return totalIncome;
+//    }
+//
+//    public int getTotalItemsSold() {
+//        return soldItems.size();
+//    }
+//
+//    public List<Product> getSoldItems() {
+//        return new ArrayList<>(soldItems);
+//    }
+//
+//    public void resetIncome() {
+//        soldItems.clear();
+//        totalIncome = 0.0;
+//    }
 }
