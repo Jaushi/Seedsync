36d9de12d5f2f6f0803f4207eb6c7c640849ff6f
mayellaaa <mariellasanchez03@gmail.com>
2025-01-29 11:24:55 +0800

    products' setter and getter

--- SeedSync/src/application/controller/ProductClass/Livestock.java
+++ SeedSync/src/application/controller/ProductClass/Livestock.java
@@ -5,17 +5,29 @@ public class Livestock extends Products{
 	private int _age;
 	
 	//getter of type
-	
+	public String getType() {
+		return _type;
+	}
 	//setter of type
-	
+	public void setType(String type) {
+		this._type = type;
+	}
 	//getter of feedDiet
-	
+	public String getFeedDiet() {
+		return _feedDiet;
+	}
 	//setter of feedDiet
-	
+	public void setFeedDiet(String feedDiet) {
+		this._feedDiet = feedDiet;
+	}
 	//getter of age
-	
+	public int getAge() {
+		return _age;
+	}
 	//setter of age
-	
+	public void setAge(int age) {
+		this._age = age;
+	}
 	
 	
 	
--- SeedSync/src/application/controller/ProductClass/Products.java
+++ SeedSync/src/application/controller/ProductClass/Products.java
@@ -7,25 +7,45 @@ abstract class Products {
 	public abstract String[] getProduct();
 	
 	//getter of pictureURL
-	
+	public String getPictureURL() {
+		return _pictureURL;
+	}
 	//setter of pictureURL
-	
+	public void setPictureURL(String pictureURL) {
+		this._pictureURL = pictureURL;
+	}
 	//getter of name
-	
+	public String getName() {
+		return _name;
+	}
 	//setter of name
-	
+	public void setName(String name) {
+		this._name = name;
+	}
 	//getter of location
-	
+	public String getLocation() {
+		return _location;
+	}
 	//setter of location
-	
+	public void setLocation(String location) {
+		this._location = location;
+	}
 	//getter of weight
-	
+	public float getWeight() {
+		return _weight;
+	}
 	//setter of weight
-	
+	public void setWeight(float weight) {
+		this._weight = weight;
+	}
 	//getter of price
-	
+	public float getPrice() {
+		return _price;
+	}
 	//setter of price
-	
+	public void setPrice(float price) {
+		this._price = price;
+	}
 }
 
 
